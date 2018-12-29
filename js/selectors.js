b4bfae2c32bf1fa99fb75a9d3c2227b9d12da907
kovner chalumo <kchalumo@gmail.com>
2018-12-29 05:09:29 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -4,8 +4,13 @@ function paragraphSelector() {
 }
 
 function lastImageSelector() {
-  return $("img:last")
+  return $("img:last");
 }
 function ninjaBabySelector(){
-  return $("#ninja-baby")
+  return $("#baby-ninja");
 }
+
+function divSelector() {
+  return $(".pics");
+}
+
